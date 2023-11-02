7f9ed0df66621e0c5d3bd79a31070de3a00218b3
BMDragon <bmweiss10@gmail.com>
2023-11-02 16:48:03 -0400

    Remove unnecessary garbage collector in test

--- backend/test/audioTest.py
+++ backend/test/audioTest.py
@@ -1,14 +1,11 @@
 import numpy as np
-import gc
 from scipy.io.wavfile import write
 
 def scale(waveform):
     return np.int16(waveform / np.max(np.abs(waveform)) * 32767)
 
-freq = 220
+freq = 360
 nSeconds = 5
 x = np.linspace(0, 1, 50)
 wavefunction = np.sin(x)
-
-gc.collect()
 write('./backend/test/test.wav', len(x)*freq, np.tile(scale(wavefunction), nSeconds*freq))
