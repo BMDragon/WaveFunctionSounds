e4ac1ce02dc4b9cbdf405376a240b275da910cba
BMDragon <bmweiss10@gmail.com>
2023-11-02 15:33:27 -0400

    Add scaling for playback on Windows

--- backend/test/audioTest.py
+++ backend/test/audioTest.py
@@ -2,9 +2,13 @@ import numpy as np
 import gc
 from scipy.io.wavfile import write
 
-freq = 100
-nSeconds = 2
+def scale(waveform):
+    return np.int16(waveform / np.max(np.abs(waveform)) * 32767)
+
+freq = 220
+nSeconds = 5
 x = np.linspace(0, 1, 50)
-waveform = np.sin(x)
+wavefunction = np.sin(x)
+
 gc.collect()
-write('./test.wav', len(x)*freq, np.tile(waveform, nSeconds*freq))
+write('./backend/test/test.wav', len(x)*freq, np.tile(scale(wavefunction), nSeconds*freq))
