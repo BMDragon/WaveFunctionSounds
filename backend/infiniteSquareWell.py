35c84f7899df2e76b15a80731bf5d87ebaf524b6
BMDragon <bmweiss10@gmail.com>
2023-11-04 21:23:20 -0400

    Simplify endpoint mismatch solution

--- backend/infiniteSquareWell.py
+++ backend/infiniteSquareWell.py
@@ -4,12 +4,12 @@ from scipy.io.wavfile import write
 def scale(waveform):
     return np.int16(waveform / np.max(np.abs(waveform)) * 32767)
 
-freq = 80
+freq = 440
 nSeconds = 5
-sampleRate = 44000
-harmonic = 13
+sampleRate = 44100
+harmonic = 2
 
-x = np.linspace(0, 1, int(sampleRate/freq))[:-1]
+x = np.linspace(0, 1, int(sampleRate/freq), False)
 k = harmonic * np.pi
 wavefunction = np.sin(k*x)
 
